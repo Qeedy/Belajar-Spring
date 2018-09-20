5326a33329f27f55a75ce0ca8a6e45d58ba664a6
qeed03 <qeed03@qeed03-Lenovo-G40-70>
2018-09-20 23:18:23 +0700

    second Commit

--- Project1/src/main/java/BelajarSpringProject1/Project1Application.java
+++ Project1/src/main/java/BelajarSpringProject1/Project1Application.java
@@ -1,4 +1,4 @@
-package BelajarSpringProject1.Project1;
+package BelajarSpringProject1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
